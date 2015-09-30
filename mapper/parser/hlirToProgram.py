e7885abbfb07636b6c17ecbaa4c58ce5278134d6
Lavanya Jose <lav@dnab404644.stanford.edu>
2015-09-29 18:57:20 -0700

    bug fix for P4->TDG: initialized program with action data widths too

--- mapper/parser/hlirToProgram.py
+++ mapper/parser/hlirToProgram.py
@@ -6,7 +6,8 @@ def getProgram(input_hlir, loglevel, pipeline_option=INGRESS_ONLY):
     pi = tp.getProgramInfo(input_hlir, loglevel, pipeline_option)
     program = Program(logicalTables=pi["num_entries"],\
 			logicalTableWidths=pi["widths"],\
-			logicalMatchDependencyList=pi["matchDependencies"],\
+                        logicalTableActionWidths=pi["action_widths"],\
+                        logicalMatchDependencyList=pi["matchDependencies"],\
 			logicalSuccessorDependencyList=pi["successorDependencies"],\
 			logicalActionDependencyList=pi["actionDependencies"],\
 			matchType=pi["match_types"],\
